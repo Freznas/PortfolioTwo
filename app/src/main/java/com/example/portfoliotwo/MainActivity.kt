c703cbc0c3fc023b155a3d5eee1a73adabc02d2d
Jocke_B <joakim198904@live.se>
2023-11-15 15:36:49 +0100

    Added text and image to profile activity. Added button functionality to all buttons, next: Update to lambda to swap between activites? v.1.3

--- app/src/main/java/com/example/portfoliotwo/MainActivity.kt
+++ app/src/main/java/com/example/portfoliotwo/MainActivity.kt
@@ -8,23 +8,35 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var projectButton: Button
-
+    private lateinit var profileButton: Button
+    private lateinit var artButton: Button
+    private lateinit var socialButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Hitta knappen med dess ID
         projectButton = findViewById(R.id.btn_project)
 
-        // Lägg till en åtgärd när knappen klickas
         projectButton.setOnClickListener {
-            // Gör något när knappen klickas
-            projectButton.setOnClickListener {
-                val intent = Intent(this, ProjectActivity::class.java)
-                startActivity(intent)
-            }
+            val intent = Intent(this, ProjectActivity::class.java)
+            startActivity(intent)
+        }
 
-            Toast.makeText(this, "Knappen klickades!", Toast.LENGTH_SHORT).show()
+        profileButton = findViewById(R.id.btn_profile)
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        artButton = findViewById(R.id.btn_graphic)
+        artButton.setOnClickListener {
+            val intent = Intent(this, ArtActivity::class.java)
+            startActivity(intent)
+        }
+        socialButton = findViewById(R.id.btn_social)
+        socialButton.setOnClickListener {
+            val intent = Intent(this, SocialActivity::class.java)
+            startActivity(intent)
         }
     }
 }
